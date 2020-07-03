36365361ee5bfbc9948b1e0478cae495541cbcb6
nataliaellem <nataliaem212@gmail.com>
2020-07-03 18:01:29 -0300

    home page function

--- lib/homepage.c
+++ lib/homepage.c
@@ -1,1 +1,14 @@
 #include "../includes/homepage.h"
+
+void home_page(){
+  char authenticated_role[8];
+  List *authenticated_user = login(authenticated_role);
+  char manager[] = "manager";
+  char client[] = "client";
+  if (strcmp(authenticated_role, manager) == 0){
+    manager_menu(authenticated_user);
+  }
+  else if (stcmp(authenticated_role, client) == 0) {
+    client_menu(authenticated_user);
+  }
+}
